3834353e4e5785eca9761f54131d72a4a3c7390d
Gaurav Rai <rai2826@gmail.com>
2024-09-14 12:01:51 +1000

    Swag Labs

--- tests/FirstTest.spec.ts
+++ tests/FirstTest.spec.ts
@@ -1,12 +1,18 @@
-import { test } from "@playwright/test";
+import { expect, test } from "@playwright/test";
 
 test("Test1", async ({ page }) => {
-  test.step("Go to the URL", async () => {
+  await test.step("Go to the URL", async () => {
     await page.goto("https://www.saucedemo.com/");
   });
-  test.step("Enter User name and pass word", async () => {
+
+  await test.step("Enter User name and pass word", async () => {
     await page.getByPlaceholder("Username").fill("standard_user");
     await page.getByPlaceholder("Password").fill("secret_sauce");
     await page.getByRole("button", { name: "Login" }).click();
+  });
+
+  await test.step("Validate the title page", async () => {
+    const title = await page.locator(".app_logo").innerText();
+    expect(title).toBe("Swag Labs");
   });
 });
